89a212884c52857dfd77e2e2a20176fd27bfb0ba
Tamim-369 <ashiqurrahmantamim369@gmail.com>
2025-02-06 15:40:57 +0600

    fixed route.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import { AuthRoutes } from '../app/modules/auth/auth.route';
 import { UserRoutes } from '../app/modules/user/user.route';
-import { RandomRoutes } from '../app/modules/random/random.route';
+
 const router = express.Router();
 
 const apiRoutes = [
-{
+  {
     path: '/user',
     route: UserRoutes,
   },
@@ -13,10 +13,6 @@ const apiRoutes = [
     path: '/auth',
     route: AuthRoutes,
   },
-  {
-    path: '/random',
-    route: RandomRoutes,
-  }
 ];
 
 apiRoutes.forEach(route => router.use(route.path, route.route));
